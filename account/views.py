f58aa1c3556c3ea1da1a00ec19f2fb500a7b4d63
Usenovaa <usenova.a00@gmail.com>
2024-02-01 12:33:01 +0600

    forgot password

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import RegistrationSerializer, ActivationSerializer, LoginSerializer
+from .serializers import RegistrationSerializer, ActivationSerializer, LoginSerializer, ChangePasswordSerializer, ForgotPasswordSerializer, ForgotPasswordCompleteSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 
 class RegistrationView(APIView):
@@ -40,10 +41,39 @@ class LoginView(ObtainAuthToken):
 
 
 class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def delete(self, request):
         user = request.user
-        print(dir(request))
-        print(request.user)
-
+        # print(dir(request))
+        # print(request.user)
+        Token.objects.filter(user=user).delete()
         return Response('Вы успешно вышли из своего аккаунта')
+    
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.set_new_password()
+        return Response('Пароль успешно изменен', status=200)
+    
+
+
+class ForgotPasswordView(APIView):
+    def post(self, request):
+        serializer = ForgotPasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.send_verification_email()
+        return Response('Сообщение для восстановления отправлено на почту',status=200)
+    
+
+class ForgotPasswordCompleteView(APIView):
+    def post(self, request):
+        serializer = ForgotPasswordCompleteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.set_new_password()
+        return Response('Пароль успешно изменен',status=200)
+    
